8fca9e8c78a5c78aa6f2879d14ee51bfa3d7618e
wendytamara <anacarlavegam@gmail.com>
2017-11-15 05:58:30 -0500

    corrigiendo errores js

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-window.onload = function() {
+document.querySelector(".content-form");
 
-
-
-var string = document.getElementsByClassName("text-content");
-
-var cont = 140;
-var contCaracteres = function(string){
-  console.log(string.length - cont);
-}
-
-contCaracteres(string);
-
-}
+input.children[1];
