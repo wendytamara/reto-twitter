048d4ca9ce383ba83f2520387f71f516e529f57c
wendytamara <anacarlavegam@gmail.com>
2017-11-16 10:11:27 -0500

    añadiendo versio 1.0 completado

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-document.querySelector(".content-form");
+var boton = document.getElementById("save");
+var contTextArea = document.getElementById("insert-text");
+var containerNewParraf = document.getElementById("container");
 
-input.children[1];
+boton.addEventListener("click", function(event) {
+   if (contTextArea.value) {
+     var parrafo = document.createElement("p");
+     var div = document.createElement("div");
+
+     parrafo.textContent = contTextArea.value;
+     div.appendChild(parrafo);
+     containerNewParraf.appendChild(div);
+     div.classList.add("content");
+
+
+     contTextArea.value = "";
+   }
+});
