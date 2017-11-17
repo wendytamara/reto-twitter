1c1c3bd63d7972c6bb79e131af5d5509e76120ca
wendytamara <anacarlavegam@gmail.com>
2017-11-16 23:29:22 -0500

    version 0.02 terminado

--- app.js
+++ app.js
@@ -3,9 +3,31 @@ window.onload = function() {
 var boton = document.getElementById("save");
 var contTextArea = document.getElementById("insert-text");
 var containerNewParraf = document.getElementById("container");
+var counterLetter = document.getElementById("number-cont");
+
+contTextArea.addEventListener("focus", function(event)  {
+     boton.classList.remove("btn");
+    boton.classList.add("btnHability");
+});
+
+contTextArea.addEventListener("blur", function(){
+    boton.classList.remove("btnHability");
+  boton.classList.add("btn");
+});
+
+
+
+
+contTextArea.addEventListener("keyup", function(){
+  var contador = 0;
+  contador = contTextArea.value.length;
+    counterLetter.value = 140 - contador;
+
+
+})
 
 boton.addEventListener("click", function(event) {
-   if (contTextArea.value) {
+   if (contTextArea.value ) {
      var parrafo = document.createElement("p");
      var div = document.createElement("div");
 
@@ -13,10 +35,13 @@ boton.addEventListener("click", function(event) {
      div.appendChild(parrafo);
      containerNewParraf.appendChild(div);
      div.classList.add("content");
+
      contTextArea.value = "";
+
    }
-   else {
-     boton.removeEventListener("click", event);
-   }
-});
+
+
+
+
+})
 };
