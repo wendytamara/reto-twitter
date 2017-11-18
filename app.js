810992c5f6d84a69a23bfc274f9367e7dca0832c
wendytamara <anacarlavegam@gmail.com>
2017-11-18 02:24:34 -0500

    añadiendo la version 0.3

--- app.js
+++ app.js
@@ -6,25 +6,60 @@ var containerNewParraf = document.getElementById("container");
 var counterLetter = document.getElementById("number-cont");
 
 contTextArea.addEventListener("focus", function(event)  {
+
+  //boton.disabled = false;
+  /*
      boton.classList.remove("btn");
-    boton.classList.add("btnHability");
+    boton.classList.add("btnHability");  */
 });
 
 contTextArea.addEventListener("blur", function(){
+
+  //boton.disabled = true;
+  /*
     boton.classList.remove("btnHability");
   boton.classList.add("btn");
+*/
+//counterLetter.value = 140
+
 });
 
 
+contTextArea.addEventListener("keyup", counterCharacters);
+function counterCharacters(event){
 
-
-contTextArea.addEventListener("keyup", function(){
   var contador = 0;
+  var letter = contTextArea.value.length;
   contador = contTextArea.value.length;
     counterLetter.value = 140 - contador;
 
+    if ( contador > 140) {
+    boton.disabled = true;
+    }
+    else if (contador >  119 && contador < 130) {
+      counterLetter.style.color = "blue";
+    }
+    else if (contador > 129 && contador < 140) {
+      counterLetter.style.color = "red";
+    }
+else {
+  boton.disabled = false;
+  counterLetter.style.color = "black";
+}
+}
+/*
+boton.addEventListener("keyup", habilityDesabilityBtn);
+function habilityDesabilityBtn(event){
 
-})
+
+    if (contador >140) {
+
+    }
+    else if (contador > 120 && contador < 140) {
+
+    }
+}
+*/
 
 boton.addEventListener("click", function(event) {
    if (contTextArea.value ) {
